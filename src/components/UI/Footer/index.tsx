c5f09aca5905fadd97270c11723a17b2c54b208e
muhammadaziz <aziz.nigmatjonov7@gmail.com>
2024-08-23 09:44:01 +0500

    fixed

--- src/components/UI/Footer/index.tsx
+++ src/components/UI/Footer/index.tsx
@@ -2,7 +2,7 @@ export const FooterUI = () => {
   return (
     <div className="mt-20">
       <div className="bg-[var(--primary)] text-white h-[70px] flex items-center">
-        <p className="container text-sm">TenetSoft Company © 2022. All Rights Reserved</p>
+        <p className="container text-sm">TenetSoft Company © 2024. All Rights Reserved</p>
       </div>
     </div>
   );
